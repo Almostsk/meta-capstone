1754f0ec3d587e6945a77c716e52d0e26a98e8a0
Dmytro Prushchak <almostsk@yahoo.com>
2024-04-22 16:34:00 +0300

    feat: create basic structure, add packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Header from "./components/header/header.component";
+import Checkout from "./routes/checkout/checkout.component";
+
 const App = () => {
   return (
-    <>
-      <header>Header</header>
-      <nav>Nav</nav>
-      <main>Main</main>
-      <footer>Footer</footer>
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index={true} element={<Home />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      primary1: "#495E57",
+      primary2: "#F4CE14",
+      secondary1: "#EE9972",
+      secondary2: "#FBDABB",
+    },
+    fontFamily: {
+      sans: ["Markazi", "sans-serif"],
+      serif: ["Karla", "serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
 
